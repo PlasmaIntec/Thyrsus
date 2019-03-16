f7a79658ded0acca1861cfcd20bd732cfc1981d0
PlasmaIntec <alphaintec@gmail.com>
2019-03-16 10:50:29 -0700

    Setup initial skeleton

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+module.exports = class App extends React.Component {
+	render() {
+		return (
+			<div>
+			RENDER
+			</div>
+		)
+	}
+}
---
+++ server/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname + '/../client/dist')));
+
+app.listen(PORT, () => {
+  console.log(`Express listening on port ${PORT}`);
+});
